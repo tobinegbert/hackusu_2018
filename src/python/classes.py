29a9a3331a372df738299503c5162dc1fe00c0ae
Alec <alecpdent@gmail.com>
2018-11-16 21:12:26 -0700

    Recipes for bread, crepes, and pancakes already added. Menu shows with each recipe listed, user picks a number corresponding to the recipe and the ingredients show. User can add their own recipes and ingredients.

--- src/python/classes.py
+++ src/python/classes.py
@@ -1,25 +1,41 @@
 class Recipe:
-    def __init__(self):
+    def __init__(self, name):
         self.__ingredients = []
+        self.__name = name
 
-    def addIngredient(self, ingredient, quantity):
+    def addIngredient(self, ingredient):
         self.__ingredients.append(ingredient)
 
-    def getIngredient(self):
+    def getAllIngredients(self):
         return self.__ingredients
+
+    def getIngredient(self, index):
+        return self.__ingredients[index]
+
+    def getName(self):
+        return self.__name
 
 
 class Cookbook:
     def __init__(self):
         self.__recipes = []
+        self.__groceryList = []
+
+    def getCookbook(self):
+        return self.__recipes
 
     def addRecipe(self, recipe):
         self.__recipes.append(recipe)
 
+    def getRecipe(self, index):
+        return self.__recipes[index]
 
-class ShoppingList:
-    def __init__(self):
-        self.__ingredients = []
+    def addToGroceryList(self, index):
+        self.__groceryList.append(self.__recipes[index])
 
-    def addIngredient(self, ingredient):
-        self.__ingredients.append(ingredient)
+    def removeFromGroceryList(self, index):
+        self.__groceryList.remove(self.__recipes[index])
+
+    def getGroceryList(self):
+        return self.__groceryList
+
--- src/python/main.py
+++ src/python/main.py
@@ -1,13 +1,65 @@
-import classes
-import xml
+from src.python.classes import Recipe
+from src.python.classes import Cookbook
 
 
 def main():
 
-    ingredient = input("Enter an ingredient: ")
-    quantity = eval(input("Enter a quantity: "))
+    cookbook = Cookbook()
 
-    classes.Recipe(ingredient, quantity)
+    bread = Recipe("Bread")
+    breadRecipe = ["3 and 1/4 cups of Flour", "3/4 Teaspoons of Salt", "1 package (1/4 ounce) of Yeast",
+                   "1 and 1/4 cups of Water"]
+    bread.addIngredient(breadRecipe)
+
+    crepe = Recipe("Crepes")
+    crepeRecipe = ["1 cup of Flour", "2 Eggs", "1/2 cup of Milk", "1/2 cup of Water", "1/4 teaspoon of Salt",
+                   "2 tablespoons of Butter (melted)"]
+    crepe.addIngredient(crepeRecipe)
+
+    pancake = Recipe("Pancakes")
+    pancakeRecipe = ["1 and 1/2 cups of Flour", "3 and 1/2 teaspoons of Baking Powder", "1 tablespoon of Sugar",
+                     "1 and 1/4 cups of Milk", "1 Egg", "# tablespoons of Butter (melted)"]
+    pancake.addIngredient(pancakeRecipe)
+
+    cookbook.addRecipe(bread)
+    cookbook.addRecipe(crepe)
+    cookbook.addRecipe(pancake)
+
+    done = False
+
+    while not done:
+        print("Choose a Recipe:")
+        for i in range(len(cookbook.getCookbook())):
+            print(str(i + 1) + ") " + str(cookbook.getRecipe(i).getName()))
+
+        print(str(len(cookbook.getCookbook()) + 1) + ") Make your own Recipe!")
+        print(str(len(cookbook.getCookbook()) + 2) + ") Exit")
+
+        userChoice = eval(input(""))
+
+        if userChoice <= len(cookbook.getCookbook()):
+            print(str(cookbook.getRecipe(userChoice - 1).getName()) + ":")
+            for i in range(len(cookbook.getRecipe(userChoice - 1).getAllIngredients())):
+                print(cookbook.getRecipe(userChoice - 1).getIngredient(i))
+        elif userChoice == len(cookbook.getCookbook()) + 1:
+            newRecipe = Recipe(input("Name your recipe: "))
+            newRecipeIngredients = []
+
+            adding = True
+            while adding:
+                userIngredient = input("Add an ingredient, or press 1 to exit and add to cookbook: ")
+                if userIngredient.strip() == "1":
+                    break
+                else:
+                    newRecipeIngredients.append(userIngredient)
+
+            cookbook.addRecipe(newRecipe)
+            print(len(cookbook.getCookbook()))
+        elif userChoice == len(cookbook.getCookbook()) + 2:
+            print("Thanks!")
+            break
+        else:
+            print("Choose again")
 
 
 main()
