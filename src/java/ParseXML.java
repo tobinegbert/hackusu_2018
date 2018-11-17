0e8cc064f7d9d27f2bff2772fabd5e18c09f2c85
tobinegbert <tobinegbert@gmail.com>
2018-11-16 19:42:59 -0700

    Started work on parsing and changed xml location

--- src/java/ParseXML.java
+++ src/java/ParseXML.java
@@ -4,19 +4,28 @@ import org.w3c.dom.*;
 
 public class ParseXML {
 
-    private static final String PATH_TO_XML = "../xml/";
+    private static final String PATH_TO_XML = "test.xml";
 
     public static void main(String[] args) {
+        try {
+            ParseXML p = new ParseXML();
+            p.parse();
+        } catch (Exception e) {
+
+        }
 
     }
 
     public void parse() throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        StringBuilder xmlStringBuilder = new StringBuilder();
-        xmlStringBuilder.append("<?xml version=\"1.0\"?> <class> </class>");
-        ByteArrayInputStream input = new ByteArrayInputStream(
-                xmlStringBuilder.toString().getBytes("UTF-8"));
-        Document doc = builder.parse(input);
+
+        File input = new File(PATH_TO_XML);
+        if(input.canRead()) System.out.println("Good");
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(input);
+        NodeList ingredientList = doc.getElementsByTagName("student");
+        for(int i = 0; i < ingredientList.getLength(); i++) {
+            System.out.println(ingredientList.item(0));
+        }
     }
 }
